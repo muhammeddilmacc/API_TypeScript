356f43315d4c301226482d96caca3d0f5a42e260
Muhammed Dilmaç <73828688+muhammeddilmacc@users.noreply.github.com>
2022-12-08 21:28:40 +0300

    initial commit

---
+++ src/resources/post/post.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+import Post from '@/resources/post/post.interface';
+
+const postSchema = new Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+        },
+
+        body: {
+            type: String,
+            required: true,
+        }
+    }
+)
+
+
+
